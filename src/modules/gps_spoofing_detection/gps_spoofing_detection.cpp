81dbc9e74ec25995d75fa259551c04ad842f4da5
elias <elias.richard.naess@gmail.com>
2026-04-14 12:54:49 +0200

    feat: add optical flow distance calculation
    close #3

--- src/modules/gps_spoofing_detection/gps_spoofing_detection.cpp
+++ src/modules/gps_spoofing_detection/gps_spoofing_detection.cpp
@@ -21,8 +21,23 @@ bool GpsSpoofingDetection::checkForOpticalFlowEstimateUpdate() {
 	if (_vehicle_optical_flow_estimate_sub.update()) {
 		const vehicle_optical_flow_vel_s &optical_flow_estimate = _vehicle_optical_flow_estimate_sub.get();
 		//save the filtered velocity information for use in comparison computation
-		_of_vel_n = optical_flow_estimate.vel_ne_filtered[0];
-		_of_vel_e = optical_flow_estimate.vel_ne_filtered[1];
+		_flow_x = optical_flow_estimate.flow_rate_compensated[0];
+		_flow_y = optical_flow_estimate.flow_rate_compensated[1];
+				// process the optical flow estimate for spoofing detection
+		_ofe_valid = true;
+
+		return true;
+	}
+
+	return false;
+}
+
+bool GpsSpoofingDetection::checkForOpticalFlowUpdate() {
+
+	if (_vehicle_optical_flow_sub.update()) {
+		const vehicle_optical_flow_s &optical_flow = _vehicle_optical_flow_sub.get();
+		//save the filtered velocity information for use in comparison computation
+		_ground_distance = optical_flow.distance_m;
 				// process the optical flow estimate for spoofing detection
 		_of_valid = true;
 
@@ -49,30 +64,33 @@ bool GpsSpoofingDetection::checkForGPSUpdate() {
 bool GpsSpoofingDetection::update() {
 	_update_count++;
 
-	_spoofing_detected = analyze_signal();
+	_spoofing_detected = analyzeSignal();
 	return _spoofing_detected;
 }
 
-bool GpsSpoofingDetection::analyze_signal() {
+float GpsSpoofingDetection::opticalFlowDistance(float ground_distance, float flow_x, float flow_y) {
+    float dx = (ground_distance * flow_x) / 10000.0f;
+    float dy = (ground_distance * flow_y) / 10000.0f;
+    return sqrt(dx * dx + dy * dy);
+}
+
+
+
+bool GpsSpoofingDetection::analyzeSignal() {
 	// Add anomly detetion logic here using optical flow etc
 	GpsSpoofingDetection::checkForOpticalFlowEstimateUpdate();
+	GpsSpoofingDetection::checkForOpticalFlowUpdate();
 	GpsSpoofingDetection::checkForGPSUpdate();
+
+
 
 	//PX4_INFO(_of_valid ? "OF valid" : "OF not valid");
 	//PX4_INFO(_gps_valid ? "GPS valid" : "GPS not valid");
 
-	if (_of_valid && _gps_valid) {
-		//PX4_INFO("HELLO");
-		const float diff_n = _gps_vel_n - _of_vel_n;
-		const float diff_e = _gps_vel_e - _of_vel_e;
-		const float vel_error = sqrtf(diff_n * diff_n + diff_e * diff_e);
-		_of_valid = false;
-		_gps_valid = false;
-
-		PX4_INFO("Velocity error: %.2f m/s", (double)vel_error);
-
-
-		return vel_error > _sensitivity_threshold;
+	if (_ofe_valid && _of_valid && _gps_valid) {
+		float of_distance = GpsSpoofingDetection::opticalFlowDistance(_ground_distance, _flow_x, _flow_y);
+		_total_distance_flow += of_distance;
+		PX4_INFO("flow_x: %f, flow_y: %f, ground_distance: %f, flow_dist: %f", (double) _flow_x, (double) _flow_y, (double) _ground_distance, (double) _total_distance_flow);
 
 	}
 
