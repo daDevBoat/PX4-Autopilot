4b8558f45f080350a9478647b93e1ae058a371a6
elias <elias.richard.naess@gmail.com>
2026-04-23 09:14:24 +0200

    stash commit (last before posting to sensor_gps)

--- src/modules/gps_spoofing_recovery/gps_spoofing_recovery.cpp
+++ src/modules/gps_spoofing_recovery/gps_spoofing_recovery.cpp
@@ -16,11 +16,6 @@ GpsSpoofingRecovery::~GpsSpoofingRecovery() = default;
 bool GpsSpoofingRecovery::update() {
 	if (_gps_spoofing_detection.spoofing_detected) {
 		//PX4_INFO("GPS SPOOFING RECOVERY ACTIVATED");
-		//param_t param_handle_gps = param_find("EKF2_GPS_CTRL");
-		//har* param_value = new char[2];
-		//param_get(param_handle_gps, param_value);
-		//PX4_INFO("Current EKF2_GPS_CTRL value: %c", param_value[0]);
-
 
 		if (!_gps_disabled) {
 			param_t param_handle = param_find("EKF2_GPS_CTRL");
@@ -31,6 +26,15 @@ bool GpsSpoofingRecovery::update() {
 				_gps_disabled = true;
 			} else {
 				PX4_ERR("Failed to find EKF2_GPS_CTRL parameter");
+			}
+
+			param_handle = param_find("EKF2_AGP0_ID");
+			if (param_handle != PARAM_INVALID) {
+				int32_t aux_gps_id = 3; // ID for aux_global_position source
+				param_set(param_handle, &aux_gps_id);
+				PX4_INFO("EKF2 auxiliary GPS set to aux_global_position");
+			} else {
+				PX4_ERR("Failed to find EKF2_AGP0_ID parameter");
 			}
 		}
 
@@ -49,5 +53,7 @@ void GpsSpoofingRecovery::publishAuxGlobalPosition() {
 	aux_pos.lat = _gps_spoofing_detection.getFlowPosition()[0];
 	aux_pos.lon = _gps_spoofing_detection.getFlowPosition()[1];
 	aux_pos.alt = 0.0; // altitude is not estimated from optical flow
+	aux_pos.id = 3;
+	aux_pos.source = 2;
 	_aux_global_position_pub.publish(aux_pos);
 }
