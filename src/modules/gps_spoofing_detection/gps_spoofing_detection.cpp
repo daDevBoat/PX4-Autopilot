f6c8d19577d90aa7ecddfcbdb61ad7e3be35838d
elias <elias.richard.naess@gmail.com>
2026-05-04 13:06:52 +0200

    feat: implemt adaptive cusum algorithm

--- src/modules/gps_spoofing_detection/gps_spoofing_detection.cpp
+++ src/modules/gps_spoofing_detection/gps_spoofing_detection.cpp
@@ -16,7 +16,9 @@
 GpsSpoofingDetection::GpsSpoofingDetection() :
 	spoofing_detected(false),
 	_sensitivity_threshold(10.0),
-	_update_count(0)
+	_update_count(0),
+	_adapt_thresh_scalar(1.5f),
+	_hits_threshold(5)
 {
 
 	param_t param_handle = param_find("EKF2_GPS_CTRL");
@@ -98,6 +100,32 @@ bool GpsSpoofingDetection::checkForMissionResultUpdate() {
 		return true;
 	}
 	return false;
+}
+
+void GpsSpoofingDetection::calculateGyroDeltaMagnitude() {
+
+	if (_vehicle_attitude_sub.update()) {
+		_prev_vehicle_attitude = _vehicle_attitude;
+		_vehicle_attitude = _vehicle_attitude_sub.get();
+	}
+
+	matrix::Quatf q(_vehicle_attitude.q);
+	matrix::Eulerf euler(q);
+
+	float roll = euler.phi();
+	float pitch = euler.theta();
+	float yaw = euler.psi();
+
+	_prev_gyro_magnitude = _gyro_magnitude;
+	_gyro_magnitude = sqrt(roll * roll + pitch * pitch + yaw * yaw);
+
+	if (_gyro_cusum_initalised) {
+		CUSUM_GYRO(_gyro_magnitude, _prev_gyro_magnitude);
+		//_output_file << _s_mag << "\t" << std::max(_s_pos, _s_neg) << "\n";
+		//_output_file.flush();
+	} else {
+		_gyro_cusum_initalised = true;
+	}
 }
 
 bool GpsSpoofingDetection::update() {
@@ -199,49 +227,48 @@ bool GpsSpoofingDetection::SSDGOF() {
 	}
 	return false;
 }
+
 bool GpsSpoofingDetection::CUSUM(double of_distance, double gps_distance) {
 	double diff = of_distance - gps_distance;
-	double baseline_diff = -0.00407; // -0.0041;
-	double k = 0.01306; // smaller k = faster detection, more false alarms  K = 0.005
-	double thresh = 25; // smaller threshold = faster detection, more false 2.2
+	double baseline_diff = -0.0041;
+	double k = 0.01306;
+	double thresh = 2.2;
 
 	PX4_INFO("CUSUM diff: %f", diff);
 
-	s_pos = std::max(0.0, s_pos + diff - baseline_diff - k);
-	s_neg = std::max(0.0, s_neg - diff + baseline_diff - k);
+	_s_pos = std::max(0.0, _s_pos + diff - baseline_diff - k);
+	_s_neg = std::max(0.0, _s_neg - diff + baseline_diff - k);
 
-	PX4_INFO("CUSUM s_pos: %f, s_neg: %f", s_pos, s_neg);
+	PX4_INFO("CUSUM s_pos: %f, s_neg: %f", _s_pos, _s_neg);
 
-	if (s_pos > thresh || s_neg > thresh) {
+	if (_s_pos > thresh || _s_neg > thresh) {
 		return true; // spoofing detected
 	}
 	return false; // no spoofing detected
 }
 
+void GpsSpoofingDetection::CUSUM_GYRO(float mag, float prev_mag) {
+	float diff = abs(mag - prev_mag);
+	float baseline_diff = 0.f;
+	float k = 0.025;
 
-bool GpsSpoofingDetection::AdaptiveCUSUM(double of_distance, double gps_distance) {
-	/*
+
+	_s_mag = std::max(0.f, _s_mag + diff - baseline_diff - k);
+	PX4_INFO("CUSUM GYRO    diff: %f    S_mag: %f", (double) diff, (double) _s_mag);
+}
+
+bool GpsSpoofingDetection::AdaptiveCUSUM(double of_distance, double gps_distance, double thresh) {
 	double diff = of_distance - gps_distance;
 	double k = 0.01306; // smaller k = faster detection, more false alarms
-	double thresh = 2.5; // smaller threshold = faster detection, more false 2
-	double baseline_diff = -0.00407; // -0.0041;
-	*/
+	double baseline_diff = -0.0041;
+	thresh = std::max(1.0, thresh);
 
-	if (_vehicle_imu_sub.update()) {
-		_prev_imu = _imu;
-		_imu = _vehicle_imu_sub.get();
+	_adapt_s_pos = std::max(0.0, _adapt_s_pos + diff - baseline_diff - k);
+	_adapt_s_neg = std::max(0.0, _adapt_s_neg - diff + baseline_diff - k);
+
+	if (_s_pos > thresh || _s_neg > thresh) {
+		return true; // spoofing detected
 	}
-
-	if (_estimator_innovations_sub.update()) {
-		_estimator_innovations = _estimator_innovations_sub.get();
-	}
-
-	if (_vehicle_attitude_sub.update()) {
-		_prev_vehicle_attitude = _vehicle_attitude;
-		_vehicle_attitude = _vehicle_attitude_sub.get();
-	}
-
-
 	return false; // no spoofing detected
 }
 
@@ -309,69 +336,27 @@ void GpsSpoofingDetection::analyzeSignal() {
 
 	GpsSpoofingDetection::checkForOpticalFlowVelUpdate();
 	GpsSpoofingDetection::checkForGPSUpdate();
-	GpsSpoofingDetection::checkForMissionResultUpdate();
 
 
-	_update_count++;
 
-	if (_ofv_valid) {
-		GpsSpoofingDetection::calculateFlowPosition();
-	}
 
 	if (_ofv_valid && _gps_valid) {
 		float dx = _optical_flow.vel_ne_filtered[0];
 		float dy = _optical_flow.vel_ne_filtered[1];
 		float flow_velocity = sqrtf(dx * dx + dy * dy);
 		float of_distance = 0.f;
+		double gps_distance = 0.0;
 
-
-		if (flow_velocity > 0.1f) {
-			of_distance = GpsSpoofingDetection::opticalFlowDistance();
-			_total_distance_flow += of_distance;
+		if (!_output_file.is_open()) {
+			PX4_ERR("Error opening output file!");
 		}
 
 		if (_gps.vel_m_s > 0.1f) {
-			double gps_distance = GpsSpoofingDetection::GPSDistance(_prev_gps.longitude_deg, _prev_gps.latitude_deg, _gps.longitude_deg, _gps.latitude_deg);
+			gps_distance = GpsSpoofingDetection::GPSDistance(_prev_gps.longitude_deg, _prev_gps.latitude_deg, _gps.longitude_deg, _gps.latitude_deg);
 			_total_distance_gps += gps_distance;
 
-			//double prev_sneg = s_neg;
-			//double prev_spos = s_pos;
-
-			// CUSUM test
-			if ((CUSUM(of_distance, gps_distance))) {
-				PX4_ERR("CUSUM GPS SPOOFING DETECTED");
-				spoofing_detected = true;
-			}
-
-			AdaptiveCUSUM(of_distance, gps_distance);
-
-			if (!_output_file.is_open()) {
-				PX4_ERR("Error opening output file!");
-			}
 
 			/*
-			_output_file << _estimator_innovations.flow[0] << "\t"
-						<< (double) of_distance - gps_distance << "\n";
-			*/
-
-
-
-			/*
-			_output_file << (int) _imu.delta_angle[0] * 1000 << "\t"
-			<< (int) _imu.delta_angle[1] * 1000 << "\t"
-			<< (int) _imu.delta_angle[2] * 1000 << "\t"
-			<< abs((double) of_distance - gps_distance) << "\n";
-			*/
-
-			_data_counter++;
-
-			matrix::Quatf q(_vehicle_attitude.q);
-  			matrix::Eulerf euler(q);
-
-  			float roll = euler.phi();
-  			float pitch = euler.theta();
-  			float yaw = euler.psi();
-
 			matrix::Quatf prev_q(_prev_vehicle_attitude.q);
   			matrix::Eulerf prev_euler(prev_q);
 
@@ -379,44 +364,79 @@ void GpsSpoofingDetection::analyzeSignal() {
   			float prev_pitch = prev_euler.theta();
   			float prev_yaw = prev_euler.psi();
 
-			double diff = abs((double) of_distance - gps_distance);
+			  double diff = abs((double) of_distance - gps_distance);
 
-			float roll_diff = abs(roll - prev_roll);
-			float pitch_diff = abs(pitch - prev_pitch);
-			float yaw_diff = abs(yaw - prev_yaw);
+			  float roll_diff = abs(roll - prev_roll);
+			  float pitch_diff = abs(pitch - prev_pitch);
+			  float yaw_diff = abs(yaw - prev_yaw);
 
-			_diff_sum += diff;
-			_roll_sum += roll_diff;
-			_pitch_sum += pitch_diff;
-			_yaw_sum += yaw_diff;
+			  _diff_sum += diff;
+			  _roll_sum += roll_diff;
+			  _pitch_sum += pitch_diff;
+			  _yaw_sum += yaw_diff;
 
 
-			if (_data_counter == 10) {
+			  if (_data_counter == 10) {
 				//_output_file << sqrt(roll_diff * roll_diff + pitch_diff * pitch_diff + yaw_diff * yaw_diff) << "\t" << diff << "\n";
 				//_output_file << sqrt(_roll_sum * _roll_sum + _pitch_sum * _pitch_sum + _yaw_sum * _yaw_sum) << "\t" << _diff_sum << "\n";
-				_output_file << _roll_sum << "\t" << _diff_sum << "\n";
+				//_output_file << _roll_sum << "\t" << _diff_sum << "\n";
 				_data_counter = 0;
 				_roll_sum = 0.f;
 				_pitch_sum = 0.f;
 				_yaw_sum = 0.f;
 				_diff_sum = 0.f;
-			}
+				}
 
+				_output_file << sqrt(roll * roll + pitch * pitch + yaw * yaw) << "\t" << sqrt(prev_roll * prev_roll + prev_pitch * prev_pitch + prev_yaw * prev_yaw) << "\n";
+				_output_file.flush();
+				*/
 
-			_output_file.flush();
+			//PX4_INFO("Errors - ang: %f, vel: %f, pos: %f", (double) _estimator_status.output_tracking_error[0], (double) _estimator_status.output_tracking_error[1], (double) _estimator_status.output_tracking_error[2]);
+
 			//PX4_INFO("IMU   x: %f, y: %f, z: %f", (double) _imu.delta_angle[0] * 1000,  (double) _imu.delta_angle[1] * 1000, (double) _imu.delta_angle[2] * 1000);
 			//PX4_INFO("Attitude: roll: %f, pitch: %f, yaw: %f", (double) roll, (double) pitch, (double) yaw);
-			PX4_INFO("gps_lat: %f, gps_lon: %f", _gps.latitude_deg, _gps.longitude_deg);
-			PX4_INFO("diff_lat: %f, diff_lon: %f", _gps.latitude_deg - _flow_lat_deg, _gps.longitude_deg - _flow_lon_deg);
+			//PX4_INFO("gps_lat: %f, gps_lon: %f", _gps.latitude_deg, _gps.longitude_deg);
+			//PX4_INFO("diff_lat: %f, diff_lon: %f", _gps.latitude_deg - _flow_lat_deg, _gps.longitude_deg - _flow_lon_deg);
 		}
 
 
-		PX4_INFO("flow_lat: %f, flow_lon: %f", _flow_lat_deg, _flow_lon_deg);
-		PX4_INFO("fx_vel: %f, fy_vel: %f, flow_dist: %f", (double) _optical_flow.vel_ne_filtered[0], (double) _optical_flow.vel_ne_filtered[1], (double) _total_distance_flow);
-		PX4_INFO("vel: %f, gps_dist: %f\n\n", (double) _gps.vel_m_s, (double) _total_distance_gps);
+		if (flow_velocity > 0.1f) {
+			of_distance = GpsSpoofingDetection::opticalFlowDistance();
+			_total_distance_flow += of_distance;
+
+			if ((CUSUM((double) of_distance, gps_distance))) {
+				_consecutive_spoofing_hits++;
+				if (_consecutive_spoofing_hits == 5) {
+					PX4_ERR("CUSUM GPS SPOOFING DETECTED");
+					//spoofing_detected = true;
+				}
+			} else {
+				_consecutive_spoofing_hits = 0;
+			}
+
+			calculateGyroDeltaMagnitude();
+
+			if (AdaptiveCUSUM((double) of_distance, gps_distance, (double) (_s_mag * _adapt_thresh_scalar))) {
+				_adaptive_consecutive_spoofing_hits++;
+				if (_adaptive_consecutive_spoofing_hits == 5) {
+					PX4_ERR("ADAPTIVE CUSUM GPS SPOOFING DETECTED");
+					//spoofing_detected = true;
+				}
+			} else {
+				_adaptive_consecutive_spoofing_hits = 0;
+			}
+
+			_output_file << _s_mag << "\t" << (double) _total_distance_flow - _total_distance_gps << "\n";
+			_output_file.flush();
+
+
+		}
+
+		PX4_INFO("gps_dist: %f opt_flow_dist: %f\n\n", (double) _total_distance_gps, (double) _total_distance_flow);
 
 		_ofv_valid = false;
 		_gps_valid = false;
 
+		_update_count++;
 	}
 }
--- src/modules/gps_spoofing_detection/gps_spoofing_detection.hpp
+++ src/modules/gps_spoofing_detection/gps_spoofing_detection.hpp
@@ -17,6 +17,7 @@
 #include <uORB/topics/vehicle_imu.h>
 #include <uORB/topics/estimator_innovations.h>
 #include <uORB/topics/vehicle_attitude.h>
+#include <uORB/topics/estimator_status.h>
 #include <lib/geo/geo.h>
 #include <lib/parameters/param.h>
 
@@ -26,8 +27,6 @@
 class GpsSpoofingDetection
 {
 public:
-	float threshold;
-
 	GpsSpoofingDetection();
 	~GpsSpoofingDetection();
 
@@ -36,10 +35,12 @@ public:
 	bool checkForOpticalFlowVelUpdate();
 	bool checkForGPSUpdate();
 	bool checkForMissionResultUpdate();
+	void calculateGyroDeltaMagnitude();
 
 	bool CUSUM(double of_distance, double gps_distance);
-	bool AdaptiveCUSUM(double of_distance, double gps_distance);
+	bool AdaptiveCUSUM(double of_distance, double gps_distance, double threshold);
 	bool SSDGOF();
+	void CUSUM_GYRO(float mag, float prev_mag);
 
 	double* getFlowPosition();
 
@@ -54,9 +55,9 @@ public:
 
 private:
 	double _sensitivity_threshold;
-
-	uint32_t _update_count;
-
+	int _update_count;
+	float _adapt_thresh_scalar;
+	int _hits_threshold;
 
 	float _total_distance_flow{0.f};
 	double _total_distance_gps{0.f};
@@ -79,7 +80,7 @@ private:
 	vehicle_imu_s _prev_imu{};
 
 	estimator_innovations_s _estimator_innovations{};
-
+	estimator_status_s _estimator_status{};
 	vehicle_attitude_s _vehicle_attitude{};
 	vehicle_attitude_s _prev_vehicle_attitude{};
 
@@ -94,10 +95,20 @@ private:
 
 	bool _flow_pos_initialised{false};
 
-	double s_pos{0.0};
-	double s_neg{0.0};
+	double _s_pos{0.0};
+	double _s_neg{0.0};
+	double _adapt_s_pos{0.0};
+	double _adapt_s_neg{0.0};
 
 	double _flow_pos[2];
+
+	int _consecutive_spoofing_hits = 0;
+	int _adaptive_consecutive_spoofing_hits = 0;
+
+	float _gyro_magnitude = 0.f;
+	float _prev_gyro_magnitude = 0.f;
+	float _s_mag = 0.f;
+	bool _gyro_cusum_initalised = false;
 
 	std::ofstream _output_file;
 
@@ -108,6 +119,7 @@ private:
 	uORB::SubscriptionData<vehicle_imu_s> _vehicle_imu_sub {ORB_ID(vehicle_imu)};
 	uORB::SubscriptionData<estimator_innovations_s> _estimator_innovations_sub {ORB_ID(estimator_innovations)};
 	uORB::SubscriptionData<vehicle_attitude_s> _vehicle_attitude_sub {ORB_ID(vehicle_attitude)};
+	uORB::SubscriptionData<estimator_status_s> _estimator_status_sub {ORB_ID(estimator_status)};
 
 	uORB::Publication<gps_spoofing_status_s> _gps_spoofing_status_pub {ORB_ID(gps_spoofing_status)};
 	uORB::Publication<vehicle_command_s> _vehicle_command_pub {ORB_ID(vehicle_command)};
